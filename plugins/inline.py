7a3d200a5cae7ae8acb1cba63919f26e372c5d11
Prasad-Madhuranga <76102607+Prasad-Madhuranga@users.noreply.github.com>
2021-03-04 21:32:25 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -59,8 +59,8 @@ async def answer(bot, query):
 def get_reply_markup(username):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [[
-        InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-        InlineKeyboardButton('Share bot', url=url),
+        InlineKeyboardButton('Search again 😅 නැවත සොයන්න', switch_inline_query_current_chat=''),
+        InlineKeyboardButton('Share bot 🤩🥰 බොටාව ශෙයා(ර්) කරන්න.', url=url),
     ]]
     return InlineKeyboardMarkup(buttons)
 
